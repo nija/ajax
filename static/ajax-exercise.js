1ecb86e10d2281e954301d104fe737b4475c3803
Hackbright Student <info@hackbrightacademy.com>
2016-04-29 13:05:46 -0700

    Initial commit

---
+++ static/ajax-exercise.js
@@ -0,0 +1,60 @@
+"use strict";
+
+
+// PART 1: SHOW A FORTUNE
+
+function showFortune(evt) {
+
+    // TODO: get the fortune and show it in the #fortune-text div
+    //$.get('/fortune', getFortune);
+
+    $.get('/fortune', function (fortune) {
+        // console.log(fortune);
+        $('#fortune-text').html(fortune);
+    });
+}
+
+// function getFortune(fortune) {
+//     console.log(fortune);
+//     $('#fortune-text').html(fortune);
+// }
+
+$('#get-fortune-button').on('click', showFortune);
+
+
+
+// PART 2: SHOW WEATHER
+
+function showWeather(evt) {
+    evt.preventDefault();
+
+    var url = "/weather.json?zipcode=" + $("#zipcode-field").val();
+
+    // TODO: request weather with that URL and show the forecast in #weather-info
+
+    $.get(url, getWeather);
+} 
+
+function getWeather(weatherDict) {
+    // console.log(weatherDict)
+    var forecast =  weatherDict['forecast']
+    $('#weather-info').html(forecast)
+}  
+
+$("#weather-form").on('submit', showWeather);
+
+
+
+
+// PART 3: ORDER MELONS
+
+function orderMelons(evt) {
+    evt.preventDefault();
+
+    // TODO: show the result message after your form
+    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+}
+
+$("#order-form").on('submit', orderMelons);
+
+
